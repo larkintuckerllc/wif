532367e6fc02025a439898490c040e6fd752e3ed
John Tucker <jtucker@spotify.com>
2021-06-02 20:28:04 -0400

    remove comments

--- wif/cli.py
+++ wif/cli.py
@@ -7,8 +7,6 @@ from google.cloud import storage
 @click.command()
 def cli():
     storage_client = storage.Client()
-    click.echo('1')
     buckets = storage_client.list_buckets()
-    click.echo('2')
     for bucket in buckets:
         click.echo(bucket.name)
